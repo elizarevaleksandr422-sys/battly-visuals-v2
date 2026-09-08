7d89a765beaff2b479764ed40ec000809334e43a
elizarevaleksandr422-sys <elizarevaleksandr422@gmail.com>
2026-09-08 08:09:30 +0300

    Create Visual Mod.java

---
+++ src/main/java/com/example/Visual Mod.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class VisualMod implements ClientModInitializer {
+
+    public static boolean enableChinaHat = false;
+    public static boolean enableCustomFog = false;
+
+    private static KeyBinding guiKeyBinding;
+
+    @Override
+    public void onInitializeClient() {
+        guiKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.visuals.open_gui",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                "category.visuals"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (guiKeyBinding.wasPressed()) {
+                if (client.player != null) {
+                    client.setScreen(new ClickGUI());
+                }
+            }
+        });
+    }
+}
